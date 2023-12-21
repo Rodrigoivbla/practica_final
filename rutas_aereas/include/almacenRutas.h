965e6cad29c4560e76e071400cebacffe14bfc69
Rene Morales <92979005+rn2k00@users.noreply.github.com>
2023-12-21 04:49:22 +0100

    Update almacenRutas.h - documentacion completada

--- rutas_aereas/include/almacenRutas.h
+++ rutas_aereas/include/almacenRutas.h
@@ -1,27 +1,50 @@
-
+/**
+  * @file almacenRutas.h
+  * @brief Fichero cabecera que almacena las rutas
+  *
+  */
 #ifndef _ALMACEN_RUTAS_H_
 #define _ALMACEN_RUTAS_H_
 
 #include "ruta.h"
 #include <map>
 
+/**
+ * @brief Clase que representa un almacén de rutas
+ */
 class Almacen_Rutas{
   private:
-    map<string, Ruta> almacen;
+    map<string, Ruta> almacen; /**< Mapa que almacena las rutas */
 
   public:
+    /**
+     * @brief Constructor por defecto
+     */
     Almacen_Rutas() {}
 
+    /**
+     * @brief Obtiene una ruta del almacén
+     * @param s Nombre de la ruta a obtener
+     * @return Ruta correspondiente al nombre proporcionado
+     */
     Ruta GetRuta(const string &s)
     {
         return almacen[s];
     }
 
+    /**
+     * @brief Inserta una ruta en el almacén
+     * @param R Ruta a insertar
+     */
     void Insertar(const Ruta &R)
     {
         almacen[R.getNombre()] = R;
     }
 
+    /**
+     * @brief Borra una ruta del almacén
+     * @param s Nombre de la ruta a borrar
+     */
     void Borrar(const string &s)
     {
         almacen.erase(s);
@@ -158,6 +181,12 @@ class Almacen_Rutas{
         return it;
     }
 
+    /**
+     * @brief Sobrecarga del operador de entrada >>
+     * @param is Stream de entrada
+     * @param AR Almacén de rutas
+     * @return Referencia al stream de entrada
+     */
     friend istream &operator>>(istream &is, Almacen_Rutas &AR)
     {
         Almacen_Rutas arlocal;
@@ -179,6 +208,12 @@ class Almacen_Rutas{
         return is;
     }
 
+    /**
+     * @brief Sobrecarga del operador de salida <<
+     * @param os Stream de salida
+     * @param AR Almacén de rutas
+     * @return Referencia al stream de salida
+     */
     friend ostream &operator<<(ostream &os, const Almacen_Rutas &AR)
     {
         os << "#Rutas" << endl;
