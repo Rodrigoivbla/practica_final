3282176401dd312c79b1641d0fe840be31bfb4ac
Rene Morales <92979005+rn2k00@users.noreply.github.com>
2023-12-21 05:03:34 +0100

    Update paises.h - documentacion finalizada

--- rutas_aereas/include/paises.h
+++ rutas_aereas/include/paises.h
@@ -1,3 +1,7 @@
+/**
+ * @file paises.h
+ * @brief Fichero cabecera que gestiona el conjunto de países utilizados
+ */
 
 #ifndef _PAISES_H_
 #define _PAISES_H_
@@ -5,157 +9,150 @@
 #include "pais.h"
 #include <set>
 #include <cassert>
+
 using namespace std;
-class Paises
-{
+
+/**
+ * @class Paises
+ * @brief Clase que gestiona un conjunto de países
+ */
+class Paises {
 private:
-    set<Pais> datos;
+    set <Pais> datos; /**< Conjunto de países */
 
 public:
+    /**
+     * @brief Constructor por defecto de la clase Paises
+     */
     Paises() {}
 
-    void Insertar(const Pais &P)
-    {
+    /**
+     * @brief Inserta un país en el conjunto de países
+     * @param P Objeto Pais a insertar
+     */
+    void Insertar(const Pais &P) {
         datos.insert(P);
     }
 
-    void Borrar(const Pais &P)
-    {
+    /**
+     * @brief Borra un país del conjunto de países
+     * @param P Objeto Pais a borrar
+     */
+    void Borrar(const Pais &P) {
         datos.erase(P);
     }
 
     class const_iterator;
-    class iterator
-    {
+
+    class iterator {
     private:
         set<Pais>::iterator p;
 
     public:
-        iterator(): p(0)
-        {}
+        iterator() : p(0) {}
 
-        iterator(const iterator &otro): p(otro.p)
-        {}
+        iterator(const iterator &otro) : p(otro.p) {}
 
-        ~iterator()
-        {}
+        ~iterator() {}
 
-        iterator& operator=(const iterator &otro)
-        {
+        iterator &operator=(const iterator &otro) {
             p = otro.p;
             return *this;
         }
 
-        const Pais& operator*() const
-        {
+        const Pais &operator*() const {
             return *p;
         }
 
-        iterator& operator++()
-        {
+        iterator &operator++() {
             p++;
             return *this;
         }
 
-        iterator& operator--()
-        {
+        iterator &operator--() {
             p--;
             return *this;
         }
 
-        bool operator!=(const iterator &otro) const
-        {
+        bool operator!=(const iterator &otro) const {
             return p != otro.p;
         }
 
-        bool operator==(const iterator &otro) const
-        {
+        bool operator==(const iterator &otro) const {
             return p == otro.p;
         }
 
         friend class Paises;
+
         friend class const_iterator;
     };
 
-    class const_iterator
-    {
+    class const_iterator {
     private:
         set<Pais>::const_iterator p;
 
     public:
-        const_iterator(): p(0)
-        {}
+        const_iterator() : p(0) {}
 
-        const_iterator(const const_iterator &otro): p(otro.p)
-        {}
+        const_iterator(const const_iterator &otro) : p(otro.p) {}
 
-        ~const_iterator()
-        {}
+        ~const_iterator() {}
 
-        const_iterator& operator=(const const_iterator &otro)
-        {
+        const_iterator &operator=(const const_iterator &otro) {
             p = otro.p;
             return *this;
         }
 
-        const Pais& operator*() const
-        {
+        const Pais &operator*() const {
             return *p;
         }
 
-        const_iterator& operator++()
-        {
+        const_iterator &operator++() {
             p++;
             return *this;
         }
 
-        const_iterator& operator--()
-        {
+        const_iterator &operator--() {
             p--;
             return *this;
         }
 
-        bool operator!=(const const_iterator &otro) const
-        {
+        bool operator!=(const const_iterator &otro) const {
             return p != otro.p;
         }
 
-        bool operator==(const const_iterator &otro) const
-        {
+        bool operator==(const const_iterator &otro) const {
             return p == otro.p;
         }
 
         friend class Paises;
     };
 
-    iterator begin()
-    {
+    iterator begin() {
         iterator it;
         it.p = datos.begin();
         return it;
     }
 
-    const_iterator begin() const 
-    {
+    const_iterator begin() const {
         const_iterator it;
         it.p = datos.begin();
         return it;
-    } 
-    
-    iterator end()
-    {
+    }
+
+    iterator end() {
         iterator it;
         it.p = datos.end();
         return it;
     }
-    
-    const_iterator end() const 
-    {
+
+    const_iterator end() const {
         const_iterator it;
         it.p = datos.end();
         return it;
-    } 
-        
+    }
+
+    // Función para encontrar un país en el conjunto según el nombre
     iterator find(const Pais &p)
     {
         iterator it;
@@ -165,6 +162,7 @@ public:
         return it;
     }
 
+    // Función para encontrar un país en el conjunto según su ubicación
     iterator find(const Punto &p)
     {
         iterator it;
@@ -174,6 +172,7 @@ public:
         return it;
     }
 
+    // Sobrecarga del operador de inserción >>
     friend istream &operator>>(istream &is, Paises &R)
     {
         Paises rlocal;
@@ -185,7 +184,7 @@ public:
         }
 
         Pais P;
-        
+
         while (is >> P)
         {
             rlocal.Insertar(P);
@@ -194,6 +193,7 @@ public:
         return is;
     }
 
+    // Sobrecarga del operador de extracción <<
     friend ostream &operator<<(ostream &os, const Paises &R)
     {
         Paises::const_iterator it;
